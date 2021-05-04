c90bfa8127a949444a17265fa96c7dd5e11271f8
Masato Takahashi <masato.takahashi.2.71828@gmail.com>
2021-05-04 12:16:18 +0900

    minor mod

--- __init__.py
+++ __init__.py
@@ -2,7 +2,10 @@ from myDSUtils.general_util import *
 from myDSUtils.ml_general_util import * 
 from myDSUtils.catboost_utils import * 
 from myDSUtils.mlflow_util import * 
-from myDSUtils.lgbm_utils import * 
+try:
+    from myDSUtils.lgbm_utils import * 
+except ImportError:
+    pass
 
 try:
     from myDSUtils.presto_utils import * 
